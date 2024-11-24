4d0dc79530a827c4f1f5fa29cb0dd7a741d412bc
Daniel Beltejar <danielbeltejar@gmail.com>
2024-11-24 17:18:43 +0100

    added prometheus metrics

--- tf-k8s-cluster/helms.tf
+++ tf-k8s-cluster/helms.tf
@@ -1,7 +1,5 @@
 module "helm_packages" {
   source = "github.com/danielbeltejar/terraform-modules.git//tfmod-helm-packages"
 
-  kubeconfig = file("~/.kube/config")
-
   helm_charts = var.helm_charts
 }
