4891ec49461eede566dd5a4b4baca435bd0a3458
Daniel Beltejar <contacto@danielbeltejar.es>
2024-12-10 19:49:17 +0100

    updated

--- tf-k8s-cluster/variables.tf
+++ tf-k8s-cluster/variables.tf
@@ -6,6 +6,19 @@ variable "namespaces_policies" {
   }))
 }
 
+variable "namespaces" {
+  description = "List of namespaces with their names and automount_service_account status."
+  type = map(object({
+    disable_automount_service_account = optional(bool, true)
+    create_environments               = optional(bool, true) 
+    create_segregated                 = optional(bool, true) 
+  }))
+  validation {
+    condition     = length(var.namespaces) > 0
+    error_message = "At least one namespace must be provided."
+  }
+}
+
 
 variable "policies" {
   type = map(object({
